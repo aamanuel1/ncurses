8d7b8b11e58a0c05161b7f7739c817cc5c155dd7
Aaron <aarmanuel@gmail.com>
2025-10-25 19:34:04 -0600

    Initial commit

---
+++ basics/init_func_example.c
@@ -0,0 +1,31 @@
+#include <curses.h>
+
+int main(void){
+
+	int ch;
+
+	initscr();
+	raw();			//raw mode on
+	keypad(stdscr, TRUE);	//Get function keys on regular screen
+	noecho();
+
+	printw("Type any character to see it in bold!\n");
+	ch = getch();
+
+	if(ch == KEY_F(1))
+		printw("F1 Key pressed");
+
+	else{
+		printw("The pressed key is ");
+		attron(A_BOLD);
+		printw("%c", ch);
+		attroff(A_BOLD);
+	}
+
+	refresh();
+	getch();
+	endwin();
+
+	return 0;
+
+}
